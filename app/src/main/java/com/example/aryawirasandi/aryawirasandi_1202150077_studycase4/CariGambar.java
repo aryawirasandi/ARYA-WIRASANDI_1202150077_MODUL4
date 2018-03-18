239651338ef406e2ce1eec2e6733e470c8cb70af
aryawirasandi <aryawirasandi7153@gmail.com>
2018-03-18 11:07:58 +0700

    first commit

---
+++ app/src/main/java/com/example/aryawirasandi/aryawirasandi_1202150077_studycase4/CariGambar.java
@@ -0,0 +1,13 @@
+package com.example.aryawirasandi.aryawirasandi_1202150077_studycase4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CariGambar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cari_gambar);
+    }
+}
